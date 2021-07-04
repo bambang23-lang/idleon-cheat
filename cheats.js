9264b15ebecfaa57fcfa10dcb665b5bb5a5c3a45
Giovani Vercauteren <4256757+iBelg@users.noreply.github.com>
2021-07-04 12:43:37 +0200

    Added minigame cheats

--- cheats.js
+++ cheats.js
@@ -3,6 +3,14 @@ const cheatState = {
 	teleports: false,
 	quickref: false,
 	tickets: false,
+	stampcost: false,
+	minigames: false,
+	minigame: {
+		mining: false,
+		catching: false,
+		fishing: false,
+		choppin: false,
+	}
 };
 let setupDone = false;
 
@@ -25,6 +33,7 @@ function setup() {
 	// setup proxies
 	setupOptionsListAccountProxy.call(this);
 	setupCurrenciesOwnedProxy.call(this);
+	setupStampCostProxy.call(this);
 }
 
 function setupOptionsListAccountProxy() {
@@ -36,7 +45,17 @@ function setupOptionsListAccountProxy() {
 			if (cheatState.quickref && Number(prop) === 34) {
 				return 0;
 			}
+			if (cheatState.minigames && Number(prop) === 33) {
+				return obj[33] || 1;
+			}
 			return Reflect.get(...arguments);
+		},
+		set: function(obj, prop, value) {
+			if (cheatState.minigames && Number(prop) === 33) {
+				if (obj[33] < value) obj[33] = value;
+				return true;
+			}
+			return Reflect.set(...arguments);
 		}
 	};
 	const proxy = new Proxy(optionsListAccount, handler);
@@ -70,6 +89,69 @@ function setupCurrenciesOwnedProxy() {
 	};
 	const proxy = new Proxy(currencies, handler);
 	bEngine.getGameAttribute('CurrenciesOwned').h = proxy;
+}
+
+function setupStampCostProxy() {
+	const bEngine = this['com.stencyl.Engine'].engine;
+	const actorEvents124 = this['scripts.ActorEvents_124'];
+	const stampCostFn = actorEvents124._customBlock_StampCostss;
+	const handler = {
+		apply: function(originalFn, context, argumentsList) {
+			if (cheatState.stampcost) {
+				const tab = argumentsList[0];
+				const index = argumentsList[1];
+				const currentStampLevel = bEngine.getGameAttribute("StampLevel")[tab][index];
+				const maxStampLevel = bEngine.getGameAttribute("StampLevelMAX")[tab][index];
+				if (currentStampLevel < maxStampLevel) {
+					return ['Money', 0];
+				}
+				return ['PremiumGem', 0];
+			}
+			return Reflect.apply(originalFn, context, argumentsList);
+		}
+	};
+	const proxy = new Proxy(stampCostFn, handler);
+	actorEvents124._customBlock_StampCostss = proxy;
+}
+
+function setupMinigameProxy() {
+	const bEngine = this['com.stencyl.Engine'].engine;
+
+	const miningGameOver = bEngine.getGameAttribute("PixelHelperActor")[4].getValue('ActorEvents_229', '_customEvent_MiningGameOver');
+	const handlerMining = {
+		apply: function(originalFn, context, argumentsList) {
+			if (cheatState.minigame.mining) {
+				return;
+			}
+			return Reflect.apply(originalFn, context, argumentsList);
+		}
+	};
+	const proxyMining = new Proxy(miningGameOver, handlerMining);	
+	bEngine.getGameAttribute("PixelHelperActor")[4].setValue('ActorEvents_229', '_customEvent_MiningGameOver', proxyMining);
+
+	const fishingGameOver = bEngine.getGameAttribute("PixelHelperActor")[4].getValue('ActorEvents_229', '_customEvent_FishingGameOver');
+	const handlerFishing = {
+		apply: function(originalFn, context, argumentsList) {
+			if (cheatState.minigame.fishing) {
+				return;
+			}
+			return Reflect.apply(originalFn, context, argumentsList);
+		}
+	};
+	const proxyFishing = new Proxy(fishingGameOver, handlerFishing);
+	bEngine.getGameAttribute("PixelHelperActor")[4].setValue('ActorEvents_229', '_customEvent_FishingGameOver', proxyFishing);
+
+	const catchingGameOver = bEngine.getGameAttribute("PixelHelperActor")[4].getValue('ActorEvents_229', '_customEvent_CatchingGameOver');
+	const handlerCatching = {
+		apply: function(originalFn, context, argumentsList) {
+			if (cheatState.minigame.catching) {
+				return;
+			}
+			return Reflect.apply(originalFn, context, argumentsList);
+		}
+	};
+	const proxyCatching = new Proxy(catchingGameOver, handlerCatching);
+	bEngine.getGameAttribute("PixelHelperActor")[4].setValue('ActorEvents_229', '_customEvent_CatchingGameOver', proxyCatching);
 }
 
 function cheat(action) {
@@ -146,6 +228,39 @@ registerCheat('teleports', function () {
 registerCheat('tickets', function () {
 	cheatState.tickets = !cheatState.tickets;
 	return `${cheatState.tickets ? 'Activated' : 'Deactived'} tickets.`
+});
+
+registerCheat('stampcost', function () {
+	cheatState.stampcost = !cheatState.stampcost;
+	return `${cheatState.stampcost ? 'Activated' : 'Deactived'} stampcost.`
+});
+
+registerCheat('minigame', function (params) {
+	// setup needs to be repeated, because currently swapping players would break the setup.
+	setupMinigameProxy.call(this);
+	if (!params || params.length === 0) {
+		cheatState.minigames = !cheatState.minigames;
+		return `${cheatState.minigames ? 'Activated' : 'Deactived'} unlimited minigames.`
+	}
+	if (params && params[0] === 'mining') {
+		cheatState.minigame.mining = !cheatState.minigame.mining;
+		return `${cheatState.minigame.mining ? 'Activated' : 'Deactived'} minigame mining.`
+	}
+	if (params && params[0] === 'fishing') {
+		cheatState.minigame.fishing = !cheatState.minigame.fishing;
+		return `${cheatState.minigame.fishing ? 'Activated' : 'Deactived'} minigame fishing.`
+	}
+	if (params && params[0] === 'catching') {
+		// cheatState.minigame.catching = !cheatState.minigame.catching;
+		// return `${cheatState.minigame.catching ? 'Activated' : 'Deactived'} minigame catching.`
+		return 'TODO.';
+	}
+	if (params && params[0] === 'choppin') {
+		// cheatState.minigame.choppin = !cheatState.minigame.choppin;
+		// return `${cheatState.minigame.choppin ? 'Activated' : 'Deactived'} minigame choppin.`
+		return 'TODO.';
+	}
+	return `${params ? params[0] : ''} not supported.`;
 });
 
 registerCheat('cheats', function () {
